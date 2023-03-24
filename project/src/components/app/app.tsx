cc8f11c6d50ff7597f50245cf2bb192ea54eb2c5
Michael Bezzaponnyy <michael.bezzaponnyy@gmail.com>
2023-03-24 16:18:15 +0300

    feat: добавит разметку main-page

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,19 @@
+import {Routes, Route} from 'react-router-dom';
+import {HelmetProvider} from 'react-helmet-async';
+
+import MainPage from '../../pages/main-page/main-page';
+
+import {AppRoute} from '../../constants';
+
+
 function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+  return (
+    <HelmetProvider>
+      <Routes>
+        <Route path={AppRoute.Main} element={<MainPage />} />
+      </Routes>
+    </HelmetProvider>
+  );
 }
 
 export default App;
