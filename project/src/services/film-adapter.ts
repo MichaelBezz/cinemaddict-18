20a94b803b5ec7a9fd69b911047a40552d87c1ef
Michael Bezzaponnyy <michael.bezzaponnyy@gmail.com>
2023-03-25 13:37:19 +0300

    feat: добавит адаптер film-to-server

--- project/src/services/film-adapter.ts
+++ project/src/services/film-adapter.ts
@@ -29,3 +29,31 @@ export const adaptFilmToClient = (film: Film): FilmAdapted => ({
     favorite: film.user_details.favorite
   }
 });
+
+export const adaptFilmToServer = (film: FilmAdapted): Film => ({
+  'id': film.id,
+  'comments': film.comments,
+  'film_info': {
+    'title': film.filmInfo.title,
+    'alternative_title': film.filmInfo.alternativeTitle,
+    'total_rating': film.filmInfo.totalRating,
+    'poster': film.filmInfo.poster,
+    'age_rating': film.filmInfo.ageRating,
+    'director': film.filmInfo.director,
+    'writers': film.filmInfo.writers,
+    'actors': film.filmInfo.actors,
+    'release': {
+      'date': film.filmInfo.release.date,
+      'release_country': film.filmInfo.release.releaseCountry
+    },
+    'runtime': film.filmInfo.runtime,
+    'genre': film.filmInfo.genre,
+    'description': film.filmInfo.description
+  },
+  'user_details': {
+    'watchlist': film.userDetails.watchlist,
+    'already_watched': film.userDetails.alreadyWatched,
+    'watching_date': film.userDetails.watchingDate,
+    'favorite': film.userDetails.favorite
+  }
+});
