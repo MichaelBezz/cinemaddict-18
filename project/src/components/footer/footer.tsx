617422b59b3720ef41a88709781aae04c16263e8
Michael Bezzaponnyy <michael.bezzaponnyy@gmail.com>
2023-03-25 11:38:04 +0300

    feat: добавит отображение количества фильмов

--- project/src/components/footer/footer.tsx
+++ project/src/components/footer/footer.tsx
@@ -1,9 +1,15 @@
+import {useAppSelector} from '../../hooks/use-app-selector';
+import {getFilms} from '../../store/films-data/selectors';
+
+
 function Footer(): JSX.Element {
+  const allFilms = useAppSelector(getFilms);
+
   return (
     <footer className="wrapper__footer footer">
       <section className="footer__logo logo logo--smaller">Cinemaddict</section>
       <section className="footer__statistics">
-        <p>130 291 movies inside</p>
+        <p>{allFilms.length} movies inside</p>
       </section>
     </footer>
   );
